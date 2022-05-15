fdd52d6123bde18c5ace88cf4457f4337d97b1bc
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-05-15 15:48:19 +0300

    Add news bubble

--- src/home.rs
+++ src/home.rs
@@ -42,12 +42,27 @@ fn spawn_home(mut commands: Commands, assets: Res<AssetServer>, game_progress: R
 		.insert(HomeMarker);
 
 	commands
+        .spawn_bundle(SpriteBundle {
+            texture: assets.load("bcman_bubble.png"),
+            transform: Transform {
+                translation: Vec3::new(-700.0, -350.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(WIDTH * 1.5, HEIGHT * 1.5)),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+		.insert(HomeMarker);
+
+	commands
         .spawn_bundle(TextBundle {
             style: Style {
                 align_self: AlignSelf::Auto,
                 position_type: PositionType::Absolute,
                 position: Rect {
-                    top: Val::Px(300.0),
+                    top: Val::Px(200.0),
                     left: Val::Px(100.0),
                     ..Default::default()
                 },
@@ -58,10 +73,10 @@ fn spawn_home(mut commands: Commands, assets: Res<AssetServer>, game_progress: R
                 TextStyle {
                     font: assets.load("FiraMono-Medium.ttf"),
                     font_size: 30.0,
-                    color: Color::WHITE,
+                    color: Color::BLACK,
                 },
                 TextAlignment {
-                    horizontal: HorizontalAlign::Center,
+                    horizontal: HorizontalAlign::Left,
                     vertical: VerticalAlign::Center,
                     ..Default::default()
                 },
