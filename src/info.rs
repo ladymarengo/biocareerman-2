6a031ee6b86b763ee94da4441f8d4823afa7d791
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-06-17 13:26:56 +0300

    Put modes into txt files

--- src/info.rs
+++ src/info.rs
@@ -3,90 +3,55 @@ use std::fs::read_to_string;
 use super::*;
 
 pub fn create_library(mut game_progress: ResMut<GameProgress>) {
-    let letters: Vec<Vec<char>> = read_to_string("assets/text/characters.txt")
+    game_progress.library.letters = read_to_string("assets/text/characters.txt")
         .unwrap()
         .split('\n')
-        .map(|s| s.split(' ').map(|c| c.chars().collect::<Vec<char>>()[0]).collect())
+        .map(|s| {
+            s.split(' ')
+                .map(|c| c.chars().collect::<Vec<char>>()[0])
+                .collect()
+        })
         .collect();
 
-    let len: Vec<Length> = read_to_string("assets/text/lengths.txt")
+    game_progress.library.len = read_to_string("assets/text/lengths.txt")
         .unwrap()
         .split('\n')
         .map(|s| {
             let tuple = s.split_once('\t').unwrap();
-            Length { min: tuple.0.parse::<usize>().unwrap(), max: tuple.1.parse::<usize>().unwrap() }
+            Length {
+                min: tuple.0.parse::<usize>().unwrap(),
+                max: tuple.1.parse::<usize>().unwrap(),
+            }
         })
         .collect();
 
-    let news: Vec<String> = read_to_string("assets/text/news.txt")
+    game_progress.library.news = read_to_string("assets/text/news.txt")
         .unwrap()
         .split('\n')
         .map(|s| s.to_string())
         .collect();
 
-    game_progress.library.letters = letters;
-    game_progress.library.len = len;
-    game_progress.library.news = news;
     game_progress.money = 1000;
     game_progress.humanness = 100;
     game_progress.day = 1;
-    game_progress.modes = vec![
-        (
-            Mode {
-                name: "Cyborg I".to_string(),
-                price: 200,
-                desc: "Eagle-eyed employee has a right for one error without a penalty."
-                    .to_string(),
-                humanness_impact: -10,
-            },
-            false,
-        ),
-        (
-            Mode {
-                name: "Smiley".to_string(),
-                price: 200,
-                desc: "Additional time for your tasks.".to_string(),
-                humanness_impact: -10,
-            },
-            false,
-        ),
-        (
-            Mode {
-                name: "Bcrrmn2".to_string(),
-                price: 400,
-                desc: "Type any button for a vowel.".to_string(),
-                humanness_impact: -40,
-            },
-            false,
-        ),
-        (
-            Mode {
-                name: "No Time To Type".to_string(),
-                price: 200,
-                desc: "Limit the length of your tasks.".to_string(),
-                humanness_impact: -30,
-            },
-            false,
-        ),
-        (
-            Mode {
-                name: "Writetyper's starterkit".to_string(),
-                price: 150,
-                desc: "Type any button for the letters 'q' and 'e'.".to_string(),
-                humanness_impact: -10,
-            },
-            false,
-        ),
-        (
-            Mode {
-                name: "Bot Remover 2000".to_string(),
-                price: 600,
-                desc: "Restores some of your humanness.".to_string(),
-                humanness_impact: 25,
-            },
-            false,
-        ),
-    ];
+
+    game_progress.modes = read_to_string("assets/text/modes.txt")
+        .unwrap()
+        .split("\n\n")
+        .map(|s| {
+            let mode = s.split('\n').collect::<Vec<&str>>();
+            (
+                Mode {
+                    name: mode[0].to_string(),
+                    price: mode[1].parse::<usize>().unwrap(),
+                    desc: mode[2].to_string(),
+                    humanness_impact: mode[3].parse::<i32>().unwrap(),
+                },
+                false,
+            )
+        })
+        .collect();
+
     game_progress.customers.random_word = read_to_string("assets/text/minigame_words.txt")
         .unwrap()
         .split('\n')
@@ -110,8 +75,8 @@ pub struct Library {
 }
 
 pub struct Length {
-	pub min: usize,
-	pub max: usize,
+    pub min: usize,
+    pub max: usize,
 }
 
 pub struct Mode {
